aeb28e78670ffd5348a4f7806bf26edca7ab8a5a
NJL7502L <g.hana1211.tako@gmail.com>
2020-12-28 22:34:15 +0900

    やっとesp01にかきこめた！

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-void setup() { pinMode(13, OUTPUT); }
-void loop() {
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(200);
+#include <ESP8266WiFi.h>
+#include "pass.h"
+
+void setup() {
+  pinMode(1, OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // wifi setup
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    WiFi.localIP();
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+void loop() {}
